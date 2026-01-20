47ec10c85b9f05ef4326afe958f58a97543654a0
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2026-01-20 10:50:54 -0700

    clean up UI

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from creater import charCreator
 chars = []
 
 def main():
-    choice = input("Select 1 to create a character and 2 to exit")
+    choice = input("Select 1 to create a character and 2 to exit: ")
     if choice == "1":
         chars.append(charCreator())
     else:
